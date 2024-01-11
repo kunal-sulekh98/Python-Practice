4eda028f510834a1382e8fc8f4bda0faa2dd4c4f
kunal-sulekh98 <97952719+kunal-sulekh98@users.noreply.github.com>
2024-01-11 22:43:35 +0530

    files exx

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+
+result = 0
+
+with open('./Work/Data/portfolio.csv', 'rt') as f:
+    h = next(f)
+    for line in f:
+        li = line.split(',')
+        share = int(li[1])
+        price = float(li[2][:-1])
+        result += share*price
+
+print(result)
