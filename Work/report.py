5546fe307acc51a4488b0353b0b98c473636c22d
kunal-sulekh98 <97952719+kunal-sulekh98@users.noreply.github.com>
2024-01-13 00:18:31 +0530

    Exercise 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,26 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+import sys
+
+if len(sys.argv) == 2:
+    fn = sys.argv[1]
+else:
+    fn = "./Work/Data/portfolio.csv"
+
+def portfolio_cost(filename):
+    '''Computes the total cost (shares*price) of a portfolio file'''
+    
+
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            portfolio.append((row[0], int(row[1]), float(row[2])))
+    return portfolio
+
+print(portfolio_cost(fn))
