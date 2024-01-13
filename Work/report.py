0874ef30122dee0bd485a09466ccad6db11f9abf
kunal-sulekh98 <97952719+kunal-sulekh98@users.noreply.github.com>
2024-01-13 14:00:15 +0530

    Ex 2.7

--- Work/report.py
+++ Work/report.py
@@ -3,24 +3,53 @@
 # Exercise 2.4
 
 import csv
-import sys
 
-if len(sys.argv) == 2:
-    fn = sys.argv[1]
-else:
-    fn = "./Work/Data/portfolio.csv"
-
-def portfolio_cost(filename):
+def portfolio(filename):
     '''Computes the total cost (shares*price) of a portfolio file'''
     
-
     portfolio = []
 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
         for row in rows:
-            portfolio.append((row[0], int(row[1]), float(row[2])))
+            stock = {}
+            stock['name'] = row[0]
+            stock['shares'] = int(row[1])
+            stock['price'] = float(row[2])
+            portfolio.append(stock)
     return portfolio
 
-print(portfolio_cost(fn))
+
+def read_prices(filename):
+    with open(filename, 'rt') as fn:
+        rows = csv.reader(fn)
+        prices = {}
+        for row in rows:
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+        return prices
+
+        
+
+from pprint import pprint
+
+
+pfolio = portfolio("./Work/Data/portfolio.csv")
+prices = read_prices("./Work/Data/prices.csv")
+
+total_cost = 0.0
+for stock in pfolio:
+    total_cost += stock['shares'] * stock['price']
+
+total_value = 0.0
+for stock in pfolio:
+    total_value += stock['shares'] * prices[stock['name']]
+
+
+print('Current value', total_value)
+print('Gain', f"{ total_value - total_cost :.2f}")
+
+#pprint(prices)
