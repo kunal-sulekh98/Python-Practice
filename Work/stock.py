6dda4de2064722b414381618179c95ce48b9d65b
kunal-sulekh98 <97952719+kunal-sulekh98@users.noreply.github.com>
2024-01-23 16:25:15 +0530

    stock restricted by slots

--- Work/stock.py
+++ Work/stock.py
@@ -1,14 +1,25 @@
 
 class Stock:
 
+    __slots__ = ('name','_shares','price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
 
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
+
+    @property
     def cost(self):
-        self.cost = self.shares * self.price
-        return self.cost
+        return self.shares * self.price
     
     def sell(self, sold):
         self.shares -= sold
